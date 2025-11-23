a2b28a73099744a2732a90fb15f6cb0813a84c06
Esinko <34211401+Esinko@users.noreply.github.com>
2025-11-23 14:41:28 +0200

    feat: extend maksukortti tests (vk4)

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,13 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_ei_lataa_jos_negatiivinen(self):
+        maksukortti_mock = Mock()
+        self.kassa.lataa(maksukortti_mock, -4)
+        maksukortti_mock.lataa.assert_not_called()
+
+    def test_kortille_lataa_jos_positiivinen(self):
+        maksukortti_mock = Mock()
+        self.kassa.lataa(maksukortti_mock, 2)
+        maksukortti_mock.lataa.assert_called_with(2)
